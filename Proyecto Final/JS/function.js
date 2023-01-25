daecfbff6965e2f0ded67d2d522ae5362ce560c1
Tomás <bordigatomas013@gmail.com>
2023-01-24 22:08:59 -0300

    Terminado

---
+++ Proyecto Final/JS/function.js
@@ -0,0 +1,10 @@
+document.getElementById('boton').onclick= function() {
+
+    document.getElementById('formulario').style.display='block'
+}
+
+document.getElementById('boton2').onclick= function() {
+
+    document.getElementById('formulario').style.display= 'none'
+
+}
